317aa88c1bb3a0f2cd02a05ee446b61b8413bb5c
ananya0809 <66442410+ananya0809@users.noreply.github.com>
2022-03-16 14:35:42 +0530

    sample configuration file for use-case

---
+++ sampleconfig.py
@@ -0,0 +1,5 @@
+class Config:
+    src_dir = "C:\\Users\\UserID\\SourceFilePath\\SourceDirectory"
+    dst_dir1 = "C:\\Users\\UserID\\DestinationFilePath1\\Sparse"
+    dst_dir2 = "C:\\Users\\UserID\\DestinationFilePath2\\Medium"
+    dst_dir3 = "C:\\Users\\UserID\\DestinationFilePath3\\Dense"
