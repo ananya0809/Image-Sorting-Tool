a78698550dc5557cf8be677ab4a4a3dfbe66b350
ananya0809 <66442410+ananya0809@users.noreply.github.com>
2022-03-19 19:05:46 +0530

    refactored code and added logic for checkpoint at every 10 images

--- sort.py
+++ sort.py
@@ -4,14 +4,52 @@ import os
 import sys
 from config import Config
 
-for jpgfile in glob.iglob(os.path.join(Config.src_dir, "*.jpg")):
-    print(jpgfile)
-    os.startfile(jpgfile)
-    the_input = str(input())
+sparselist = []
+mediumlist = []
+denselist = []
 
+def classify(jpgfile, the_input):
     if the_input == "s":
-        shutil.move(jpgfile, Config.dst_dir1)
+        sparselist.append(jpgfile)
+        print(sparselist)
     if the_input == "m":
-        shutil.move(jpgfile, Config.dst_dir2)
+        mediumlist.append(jpgfile)
+        print(mediumlist)
     if the_input == "d":
-        shutil.move(jpgfile, Config.dst_dir3)
+        denselist.append(jpgfile)
+        print(denselist)
+
+def load():
+    count = 1
+    # print(count)
+    for jpgfile in glob.iglob(os.path.join(Config.src_dir, "*.jpg")):
+        print(count)
+        if count <= 10:
+            print(jpgfile)
+            os.startfile(jpgfile)
+            the_input = str(input())
+            classify(jpgfile,the_input)
+            count += 1
+        else:
+            break
+    print("Images in sparse list: ")
+    print(sparselist)
+    print("Images in medium list: ")
+    print(mediumlist)
+    print("Images in dense list: ")
+    print(denselist)
+    print("moving previous 10 images to respective directories...")
+    move()
+
+def move():
+    for index, file in enumerate(sparselist):
+        shutil.move(file, Config.dst_dir1)
+    for index, file in enumerate(mediumlist):
+        shutil.move(file, Config.dst_dir2)
+    for index, file in enumerate(denselist):
+        shutil.move(file, Config.dst_dir3)
+
+for jpgfile in glob.iglob(os.path.join(Config.src_dir, "*.jpg")):
+    while True:
+        load()
+
